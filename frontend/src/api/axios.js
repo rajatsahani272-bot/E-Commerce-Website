a959bb864c565b6acb4a77bece69e338a67a268e
Rajat Sahani <rajatsahani272@gmail.com>
2026-06-06 15:55:11 +0530

    frontend setup beforte deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api =axios.create({
-    baseURL:'http://localhost:5001/api',
+    baseURL:'https://e-commerce-website-75gq.onrender.com/api',
 })
 
 export default api;
